9897a051c74a5190f89124008017588b6e96c75c
Joowon Jang <72129693+juwon98@users.noreply.github.com>
2024-01-16 15:40:18 +0900

    [Gold IV] Title: 약수의 합, Time: 92 ms, Memory: 9708 KB -BaekjoonHub

---
+++ 백준/Gold/17425. 약수의 합/약수의 합.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    long long sum[1000001]={0,};
+    int t;
+    long long ans=0;
+    
+    for(int i=1; i<1000001; i++) {
+        for(int j=i; j<1000001; j+=i) {
+            sum[j]+=i;
+        }
+    }
+    for(int i=1; i<1000001; i++) {
+        sum[i]+=sum[i-1];
+    }
+    
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        
+        cout << sum[n] << "\n";
+    }
+
+    return 0;
+}
