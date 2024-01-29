b025d04a7e73af8603aa6c4055963f9f36f6240b
Joowon Jang <72129693+juwon98@users.noreply.github.com>
2024-01-29 16:43:29 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 4 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+char arr[400][400];
+int n;
+
+void recursion(int n, int r, int c) {
+    for(int i=r; i<r+4*n-3; i++) {
+        if(i==r || i==r+4*n-4) {
+            for(int j=c; j<c+4*n-3; j++) {
+                arr[i][j]='*';
+            }
+        }
+        else {
+            arr[i][c]='*';
+            arr[i][c+4*n-4]='*';
+        }
+    }
+    
+    if(n>0) recursion(n-1, r+2, c+2);
+}
+
+int main()
+{
+    cin >> n;
+    
+    for(int i=0; i<4*n-3; i++) {
+        for(int j=0; j<4*n-3; j++) {
+            arr[i][j]=' ';
+        }
+    }
+    
+    recursion(n, 0, 0);
+    
+    for(int i=0; i<4*n-3; i++) {
+        for(int j=0; j<4*n-3; j++) {
+            cout << arr[i][j];
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
