1f7379203528c729f521633942f0eadf627c6a7a
Joowon Jang <72129693+juwon98@users.noreply.github.com>
2024-01-30 19:30:38 +0900

    [Silver II] Title: 마인크래프트, Time: 128 ms, Memory: 2876 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.cc
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.cc
@@ -4,47 +4,45 @@ using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     
-    int n, m, b;
-    int lt = 0x7f7f7f7f;
-    int ground[500][500];
-    int heightest = 0;
+    int n, m ,b;
+    int map[500][500];
+    int mint=0x7f7f7f7f;
+    int highest=0;
     
     cin >> n >> m >> b;
     for(int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
-            cin >> ground[i][j];
+            cin >> map[i][j];
         }
     }
     
     for(int h=0; h<=256; h++) {
-        int build=0;
         int crash=0;
+        int build=0;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                int height = ground[i][j] - h;
-                if(height > 0) {
+                int height=map[i][j]-h;
+                if(height>=0) {
                     crash+=height;
                 }
-                else if(height < 0) {
+                else {
                     build-=height;
                 }
             }
         }
         
-        if(crash + b >= build) {
-            int t = crash*2 + build;
-            if(lt >= t) {
-                lt = t;
-                heightest = h;
+        if(crash+b>=build) {
+            int t=crash*2+build;
+            if(mint>=t) {
+                mint=t;
+                highest=h;
             }
         }
     }
     
-    cout << lt << " " << heightest;
+    cout << mint << " " << highest;
 
     return 0;
 }
