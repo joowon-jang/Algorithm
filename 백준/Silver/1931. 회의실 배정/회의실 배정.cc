4bfcaef39f08475334b779690b5154c2d5c2c7ef
Joowon Jang <72129693+juwon98@users.noreply.github.com>
2024-02-16 16:32:30 +0900

    [Silver I] Title: 회의실 배정, Time: 32 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -15,6 +15,7 @@ bool compare(pair<int,int> a, pair<int,int> b) {
 
 int main()
 {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     int n;
     vector<pair<int,int>> conf;
     int ans=0;
