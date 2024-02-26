824c7158b64d47879699333a706f6e6e69a6cd4d
Joowon Jang <72129693+juwon98@users.noreply.github.com>
2024-02-26 14:18:36 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 384 ms, Memory: 22920 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -16,7 +16,7 @@ int main()
     cin >> n >> m >> k >> x;
     
     vector<vector<int>> graph(n+1,vector<int>(0));
-    vector<int> dist;
+    vector<int> dist(n+1,INF);
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
     
     for(int i=0; i<m; i++) {
@@ -25,22 +25,19 @@ int main()
         graph[from].push_back(to);
     }
     
-    for(int i=0; i<=n; i++) {
-        dist.push_back(INF);
-    }
-    pq.push({x,0});
+    pq.push({0,x});
     dist[x]=0;
     
     while(!pq.empty()) {
-        int curr=pq.top().first;
-        int cost=pq.top().second;
+        int curr=pq.top().second;
+        int cost=pq.top().first;
         pq.pop();
         
         for(int i=0; i<graph[curr].size(); i++) {
             int next=graph[curr][i];
             if(dist[next] > cost+1) {
                 dist[next] = cost+1;
-                pq.push({next,cost+1});
+                pq.push({cost+1,next});
             }
         }
     }
