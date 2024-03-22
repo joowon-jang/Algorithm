1f0956b3de8881b1e125468329c6b0aacf66ca90
Joowon Jang <72129693+juwon98@users.noreply.github.com>
2024-03-22 19:03:34 +0900

    [Gold V] Title: 강의실 배정, Time: 192 ms, Memory: 5108 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    vector<pair<int,int>> v;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        int s,e;
+        cin >> s >> e;
+        v.push_back({s,e});
+    }
+    
+    sort(v.begin(), v.end());
+    pq.push(v[0].second);
+    
+    for(int i=1; i<n; i++) {
+        pq.push(v[i].second);
+        if(pq.top() <= v[i].first) pq.pop();
+    }
+    
+    cout << pq.size();
+    
+    return 0;
+}
