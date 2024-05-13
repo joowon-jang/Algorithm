58135c8f88b988205c3745f9419c5943dc54ee60
JoowonJang <72129693+joowon-jang@users.noreply.github.com>
2024-05-13 17:48:37 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    
+    int student[32] = {0,};
+    
+    for(int i = 0; i < lost.size(); i++){
+        student[lost[i]]--;
+    }
+    
+    for(int i = 0; i < reserve.size(); i++){
+        student[reserve[i]]++;
+    }
+    
+    for(int i = 1; i <= n; i++){
+        if(student[i] == -1){
+            if(student[i - 1] == 1){
+                student[i - 1] = 0;
+                student[i] = 0;
+            }
+            else if(student[i + 1] == 1){
+                student[i + 1] = 0;
+                student[i] = 0;
+            }
+        }   
+            
+        if(student[i] >= 0) answer++;
+    }
+    
+    return answer;
+}
