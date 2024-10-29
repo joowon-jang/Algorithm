8336b5118d904a90e646fb8e4397d1ef606a1207
JoowonJang <72129693+joowon-jang@users.noreply.github.com>
2024-10-29 19:42:17 +0900

    [level 2] Title: 튜플, Time: 48.80 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,19 +1,17 @@
 function solution(s) {
     let answer = [];
     
-    let arr = s.split('},{');
-    arr[0] = arr[0].slice(2);
-    arr[arr.length - 1] = arr[arr.length - 1].slice(0,-2);
+    let arr = s.slice(2, -2).split('},{');
     
     for(let i=0; i<arr.length; i++) {
-        arr[i] = arr[i].split(',');
+        arr[i] = arr[i].split(',').map(el => Number(el));
     }
     
     arr.sort((a, b) => a.length < b.length ? -1 : a.length === b.length ? 0 : 1);
     
     for(let i=0; i<arr.length; i++) {
-        filtered = arr[i].filter(el => !answer.includes(Number(el)));
-        filtered.forEach(el => answer.push(Number(el)));
+        filtered = arr[i].filter(el => !answer.includes(el));
+        filtered.forEach(el => answer.push(el));
     }
     
     return answer;
