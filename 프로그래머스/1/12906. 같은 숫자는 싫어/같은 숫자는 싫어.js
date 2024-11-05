80f1ee950bdf9a6b0b5d97effc5d88e490db374a
JoowonJang <72129693+joowon-jang@users.noreply.github.com>
2024-11-05 14:39:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 40.14 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,8 +1,9 @@
 function solution(arr)
 {
     let answer = [arr[0]];
+    const len = arr.length;
     
-    for(let i=1; i<arr.length; i++) {
+    for(let i=1; i<len; i++) {
         if(arr[i-1] !== arr[i]) answer.push(arr[i]);
     }
     
