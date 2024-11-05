bc3f759e22b74374009de60991fd602d4660bc0e
JoowonJang <72129693+joowon-jang@users.noreply.github.com>
2024-11-05 13:20:09 +0900

    [level 1] Title: 폰켓몬, Time: 0.56 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,5 @@
 function solution(nums) {
-    let answer = 0;
-    const dict = new Map();
+    const set = new Set(nums);
     
-    for(const num of nums) {
-        dict.set(num, (dict.get(num) || 0) + 1);
-    }
-    
-    if(nums.length / 2 >= dict.size) answer = dict.size;
-    else answer = nums.length / 2;
-    
-    return answer;
+    return nums.length / 2 >= set.size ? set.size : nums.length / 2;
 }
