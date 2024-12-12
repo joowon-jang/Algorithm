b5ea03ef8d0d6ee230550499b54e384469dbc3cd
JoowonJang <72129693+joowon-jang@users.noreply.github.com>
2024-12-12 18:36:00 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 34.17 ms, Memory: 42.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,29 @@
+function solution(queue1, queue2) {
+    const len = queue1.length + queue2.length;
+    let answer = 0;
+    let sum1 = queue1.reduce((acc, num) => acc + num);
+    let sum2 = queue2.reduce((acc, num) => acc + num);
+    let idx1 = 0;
+    let idx2 = 0;
+    
+    while(sum1 !== sum2) {
+        if(sum1 > sum2) {
+            const tmp = queue1[idx1];
+            queue2.push(tmp);
+            sum1 -= tmp;
+            sum2 += tmp;
+            idx1++;
+        }
+        else {
+            const tmp = queue2[idx2];
+            queue1.push(tmp);
+            sum2 -= tmp;
+            sum1 += tmp;
+            idx2++;
+        }
+        answer++;
+        if(idx1 > len || idx2 > len) return -1;
+    }
+    
+    return answer;
+}
