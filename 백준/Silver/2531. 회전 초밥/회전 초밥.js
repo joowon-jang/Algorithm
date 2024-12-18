b44a3d87bd8b48f53b4643a6fc58eed75a300df0
JoowonJang <72129693+joowon-jang@users.noreply.github.com>
2024-12-18 16:04:43 +0900

    [Silver I] Title: 회전 초밥, Time: 176 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.js
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.js
@@ -1,33 +1,38 @@
-const readline = require("readline");
+const fs = require("fs");
+// 입력 파일 읽기
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+// 입력 파싱
+const [N, d, k, c] = input[0].split(" ").map(Number);
+const sushies = input.slice(1).map(Number);
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// 결과 출력
+console.log(solution(N, d, k, c, sushies));
 
-let input = [];
-
-rl.on('line', (line) => {
-    input.push(line);
-}).on('close', () => {
-    const [N, d, k ,c] = input[0].split(' ').map(Number);
-    let sushies = [];
-    for(let i = 1; i < input.length; i++) {
-        sushies.push(Number(input[i]));
-    }
-    
-    console.log(solution(N, d, k, c, sushies));
-})
-
+// 풀이
 function solution(N, d, k, c, sushies) {
-    let answer = 0;
-    const originLen = sushies.length;
-    sushies.push(...sushies);
-    for(let i = 0; i < originLen; i++) {
-        const s = new Set(sushies.slice(i, i + k));
-        if(s.has(c)) answer = Math.max(s.size, answer);
-        else answer = Math.max(s.size + 1, answer);
-    }
-    
-    return answer;
+  let answer = 0;
+  
+  const extendedSushies = sushies.concat(sushies);
+
+  const sushiMap = new Map();
+  let kind = 0;
+
+  for (let i = 0; i < k; i++) {
+    sushiMap.set(extendedSushies[i], (sushiMap.get(extendedSushies[i]) || 0) + 1);
+  }
+  answer = sushiMap.size + (sushiMap.has(c) ? 0 : 1);
+
+  for (let i = 0; i < N; i++) {
+    const left = extendedSushies[i];
+    const right = extendedSushies[i + k];
+
+    sushiMap.set(left, sushiMap.get(left) - 1);
+    if (sushiMap.get(left) === 0) sushiMap.delete(left);
+
+    sushiMap.set(right, (sushiMap.get(right) || 0) + 1);
+
+    answer = Math.max(answer, sushiMap.size + (sushiMap.has(c) ? 0 : 1));
+  }
+
+  return answer;
 }
