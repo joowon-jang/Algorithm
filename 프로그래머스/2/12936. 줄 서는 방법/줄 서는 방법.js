18c1729c1fcb24a563373371a116401b3126c6a3
JoowonJang <72129693+joowon-jang@users.noreply.github.com>
2024-12-20 22:28:21 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.22 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -1,15 +1,13 @@
 function solution(n, k) {
     const answer = [];
-    let people = Array.from({ length: n }, (_, i) => i + 1);
+    const people = Array.from({ length: n }, (_, i) => i + 1);
     let caseNum = people.reduce((acc, person) => acc * person, 1);
     k--;
     
     while (answer.length < n) {
         caseNum = caseNum / people.length;
-        const tmp = people[Math.floor(k / caseNum)];
-        answer.push(tmp);
+        answer.push(...people.splice(Math.floor(k / caseNum), 1));
         k = k % caseNum;
-        people = people.filter(person => person !== tmp);
     }
 
     return answer;
